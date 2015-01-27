28c0d6b10bd234a9720611804f384b454ae45196
jabbersefton <jabbersefton@users.noreply.github.com>
2015-01-27 16:19:17 +0000

    fixed box sizes

--- controls.js
+++ controls.js
@@ -1,25 +1,19 @@
 /* Javascript/jquery controls for  etch a sketch project */
 $container = $('<div id="container"></div>');
 $(document).ready(function(){
-    $("body").prepend($container)
-    
-    $gridRow ='<div class="gridRow"></div>'; 	
-	for(  i = 0; i < 4; i++){
+    $("body").prepend($container)   
+        
+});
+	
+$(document).ready(function(){   
+ $gridRow ='<div class="gridRow"></div>'; 	
+	for(  i = 0; i < 16; i++){
         $("#container").prepend($gridRow);       
     
 }
 $box = '<div class="box"></div>';
-for( i = 0; i < 4; i++){
-        $(".gridRow").prepend($box);
-        
+for( i = 0; i < 16; i++){
+        $(".gridRow").prepend($box);        
     
-}
-	
-	
-		
-		
-	   
-    
-    
+}	   
 });
-	
