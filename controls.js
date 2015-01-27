16d6ccdbca66fcf84a0954a5359b19aadd282644
jabbersefton <jabbersefton@users.noreply.github.com>
2015-01-27 15:17:47 +0000

    single grid box added in jquery

--- controls.js
+++ controls.js
@@ -1,1 +1,8 @@
 /* Javascript/jquery controls for  etch a sketch project */
+$container = $('<div id="container"></div>');
+$(document).ready(function(){
+    $("body").prepend($container);
+    $box = '<div class="box"></div>';
+    $("#container").prepend($box);
+});
+	
