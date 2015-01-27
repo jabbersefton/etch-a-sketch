87d4830ddbe09ce8601d54ad360b5b56074ed39f
jabbersefton <jabbersefton@users.noreply.github.com>
2015-01-27 16:01:39 +0000

    Added 16x16 grid

--- controls.js
+++ controls.js
@@ -2,11 +2,16 @@
 $container = $('<div id="container"></div>');
 $(document).ready(function(){
     $("body").prepend($container)
-	$box = '<div class="box"></div>';
-	
-	for(var i = 0; i < 4; i++){
     
-    $("#container").prepend($box);
+    $gridRow ='<div class="gridRow"></div>'; 	
+	for(  i = 0; i < 4; i++){
+        $("#container").prepend($gridRow);       
+    
+}
+$box = '<div class="box"></div>';
+for( i = 0; i < 4; i++){
+        $(".gridRow").prepend($box);
+        
     
 }
 	
