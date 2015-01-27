dd69cbc02a15b66ac2b422e3bb6f34c293222d26
jabbersefton <jabbersefton@users.noreply.github.com>
2015-01-27 15:36:08 +0000

    added first row

--- controls.js
+++ controls.js
@@ -1,8 +1,20 @@
 /* Javascript/jquery controls for  etch a sketch project */
 $container = $('<div id="container"></div>');
 $(document).ready(function(){
-    $("body").prepend($container);
-    $box = '<div class="box"></div>';
+    $("body").prepend($container)
+	$box = '<div class="box"></div>';
+	
+	for(var i = 0; i < 4; i++){
+    
     $("#container").prepend($box);
+    
+}
+	
+	
+		
+		
+	   
+    
+    
 });
 	
